4c4614cf607b040b3084cb396cbb92dbb43bf147
Sarabjot <sarabsingh676@gmail.com>
2025-03-28 14:15:20 +0530

    DS13

--- DayTwo/Question1.js
+++ DayTwo/Question1.js
@@ -1,11 +1,15 @@
 // Write a function to generate an ID of a defined length. ID will be alphanumeric. e.g. generateID(5) ⇒ a31cx, generateeID(3) ⇒ 1x0.
-function generateId(Length){
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnoopqrstuvxyz0123456789';
-    var charactersLength = characters.length;
-    for (var i = 0; i < Length; i++){
-        result += characters.charAt( charactersLength);
+function generateId(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
-    console.log(generate());
-}
+  }
+  
+  console.log(generateId(5));
+  console.log(generateId(3));
+  console.log(generateId(10));
+  console.log(generateId(15));
